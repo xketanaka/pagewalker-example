28353c3b0f13174a0e21a1d39b99eca7441ae46d
tanaka ken <tanaka-ken@netlab.jp>
2025-01-08 10:49:52 +0900

    Add fileUploadExample

--- app/routes/file_upload_example.js
+++ app/routes/file_upload_example.js
@@ -1,19 +1,42 @@
+const uploadConfig = { dest: __dirname + '/../public/uploads/' };
 const multer = require('multer');
-const upload = multer({ dest: __dirname + '/public/uploads/' })
+const path = require('path');
+const upload = multer(uploadConfig);
+const fs = require('fs');
 
 module.exports = {
   GET: {
     "/file_upload_example": (req, res)=>{
-      res.renderLayout("/file_upload_example/index");
+      let files = fs.readdirSync(uploadConfig.dest);
+      res.renderLayout("/file_upload_example/index", { files: files });
     },
     "/file_upload_example/done": (req, res)=>{
-      res.renderLayout("/file_upload_example/done", { filename: req.query.filename });
+      let files = fs.readdirSync(uploadConfig.dest);
+      res.renderLayout("/file_upload_example/index", { files: files, message: `"${req.query.filename}" has been uploaded.` });
+    },
+    "/file_upload_example/download": (req, res)=>{
+      let files = fs.readdirSync(uploadConfig.dest);
+      if (files.includes(req.query.filename)) {
+        res.setHeader('Content-disposition', `attachment; filename*=utf8''${encodeURIComponent(req.query.filename)}`);
+        res.setHeader('Content-type', "application/octet-stream");
+
+        fs.createReadStream(path.join(uploadConfig.dest, req.query.filename)).pipe(res);
+      } else {
+        res.renderLayout("/file_upload_example/index", { files: files, message: `"${req.query.filename}" is not found.` });
+      }
     },
   },
   POST: {
     "/file_upload_example/upload": [
       upload.single('file'), (req, res)=>{
-        res.redirect(`/file_upload_example/done?filename=${req.file.originalname}`);
+        fs.rename(req.file.path, uploadConfig.dest + req.file.originalname, (err)=>{
+          if (!err) {
+            res.redirect(`/file_upload_example/done?filename=${req.file.originalname}`);
+          } else {
+            console.log(err);
+            res.renderLayout("/index", { message: `Unexpected error has occurred, ${err}` });
+          }
+        });
       }
     ]
   }
