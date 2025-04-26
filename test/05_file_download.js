cf0e2c27b1b1b74f211f6873a49b2ddcf3929fa3
tanaka ken <tanaka-ken@netlab.jp>
2025-04-26 22:04:45 +0900

    Chnage downloading PDF to downloading JPG.

--- test/05_file_download.js
+++ test/05_file_download.js
@@ -16,7 +16,52 @@ describe("05.File Download Example", ()=>{
     })
   })
 
-  it("1. file download pdf, attachment", async ()=>{
+  it("1. file download jpg, attachment", async ()=>{
+
+    let file = await page.waitForDownload(async ()=>{
+      await page.find("#jpg-form button").click();
+    })
+
+    assert.equal(file.filename, "default.jpg");
+    assert.equal(Buffer.compare(
+      await fileReadPromise(file.savedFilePath),
+      await fileReadPromise(path.join(__dirname, '../app/views/file_download_example/download_file.jpg'))
+    ), 0);
+  });
+
+  it("2. file download jpg, inline", async ()=>{
+
+    await page.find("#jpg-form input[type=radio]").haveValue("inline").check();
+
+    let file = await page.waitForDownload(async ()=>{
+      await page.find("#jpg-form button").click();
+    })
+
+    assert.equal(file.filename, "default.jpg");
+    if (config.browser != "electron") {
+      assert.equal(Buffer.compare(
+        await fileReadPromise(file.savedFilePath),
+        await fileReadPromise(path.join(__dirname, '../app/views/file_download_example/download_file.jpg'))
+      ), 0);
+    }
+    else {
+      // wait
+      await new Promise((resolve) => { setTimeout(resolve, 100) });
+    }
+  });
+
+  it("3. file download jpg, other tab", async ()=>{
+
+    await page.find("#jpg-form input[type=radio]").haveValue("other-inline").check();
+    const newWin = await page.waitForNewWindow(async ()=>{
+
+      await page.find("#jpg-form button").click();
+    })
+
+    await newWin.close();
+  });
+
+  it("4. file download pdf, attachment", async ()=>{
 
     let file = await page.waitForDownload(async ()=>{
       await page.find("#pdf-form button").click();
@@ -29,7 +74,7 @@ describe("05.File Download Example", ()=>{
     ), 0);
   });
 
-  it("2. file download pdf, inline", async ()=>{
+  xit("5. file download pdf, inline", async ()=>{
 
     await page.find("#pdf-form input[type=radio]").haveValue("inline").check();
 
@@ -40,24 +85,21 @@ describe("05.File Download Example", ()=>{
     if (config.browser != "electron") {
       assert.equal(file.filename, "default.pdf");
       assert(!file.savedFilePath);
+      await new Promise((resolve) => { setTimeout(resolve, 1000) });
     }
     else {
       // wait
       await new Promise((resolve) => { setTimeout(resolve, 100) });
     }
-
- //   await page.assertScreen("inline content");
   });
 
-  it("3. file download pdf, other tab", async ()=>{
+  xit("6. file download pdf, other tab", async ()=>{
 
     await page.find("#pdf-form input[type=radio]").haveValue("other-inline").check();
-
     const newWin = await page.waitForNewWindow(async ()=>{
+
       await page.find("#pdf-form button").click();
     })
-
-    // await newWin.page.assertScreen("other tab content");
 
     await newWin.close();
   });
