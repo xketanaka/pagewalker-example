24d54c142f1ac917c207bee186d456d4ada9f1d1
tanaka ken <tanaka-ken@netlab.jp>
2025-05-30 14:37:36 +0900

    Add test for textIncludes

--- test/08_table.js
+++ test/08_table.js
@@ -32,13 +32,18 @@ describe("08.Table Example", ()=>{
   it("3. Dom operation", async ()=>{
     await page.find('td').haveContent("Apple").parent().find("input[type=number]").fillIn("10");
     await page.find('td').haveContent("Orange").parent().find("input[type=number]").fillIn("20");
+    await page.find('td').haveContent("Banana").parent().find("input[type=number]").fillIn("100");
 
     await page.find(`input[type=submit]`).valueMatch(/apply/).click();
     await page.waitForPageLoad();
 
     assert.strictEqual(await page.find('td').textMatch(/Apple/).closest("tr").find("td:nth-child(3)").text(), "10");
     assert.strictEqual(await page.find('td').textMatch(/Orange/).closest("tr").find("td:nth-child(3)").text(), "20");
+    assert.strictEqual(await page.find('td').textMatch(/Banana/).closest("tr").find("td:nth-child(3)").text(), "100");
     assert.strictEqual(await page.find('td').textMatch(/Pea/).count(), 2);
+    assert.strictEqual(await page.find('td').textInclude("Pea").count(), 2);
     assert.strictEqual(await page.find('td').find(`td => td.textContent.match(/Pea/)`).count(), 2);
+    assert.strictEqual(await page.find('input').valueInclude("10").count(), 2);
+    assert.strictEqual(await page.find('input').haveValue("10").count(), 1);
   })
 })
